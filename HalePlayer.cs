2300a89bf457d391eef8ebdfb9096f2f1e90423b
qufnr <qufnr@icloud.com>
2024-11-12 00:44:10 +0900

    hale player : 클래스 추가

---
+++ HalePlayer.cs
@@ -0,0 +1,15 @@
+﻿namespace FreakStrike2
+{
+    public partial class FreakStrike2
+    {
+        public Dictionary<int, bool> IsHalePlayer = new Dictionary<int, bool>();
+        
+        /**
+         * 헤일 플레이어 찾기
+         */
+        private void FindHaleFromServerSlots()
+        {
+            
+        }
+    }
+}
