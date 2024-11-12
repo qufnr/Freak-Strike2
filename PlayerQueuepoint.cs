57b75b3ede31676f99eee63d9290c1c598c7ee0a
qufnr <qufnr@kakao.com>
2024-11-13 01:38:03 +0900

    queuepoint : Queuepoint 관련 처리 메소드 추가

---
+++ PlayerQueuepoint.cs
@@ -0,0 +1,12 @@
+﻿namespace FreakStrike2;
+
+public partial class FreakStrike2
+{
+    /**
+     * 클라이언트 퇴장 시 Queuepoint 초기화 (OnClientDisconnect)
+     */
+    private void ResetQueuepointOnClientDisconnect(int client)
+    {
+        _queuepoint.SetPlayerQueuepoint(client, 0);
+    }    
+}
