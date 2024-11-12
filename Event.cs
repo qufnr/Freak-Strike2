fbf6ccb169b921190507512fe6e0d21eabc8efc5
qufnr <qufnr@kakao.com>
2024-11-13 01:35:52 +0900

    event : 이벤트 로직 수정, 주석 추가

--- Event.cs
+++ Event.cs
@@ -1,67 +1,111 @@
-﻿using CounterStrikeSharp.API;
-using CounterStrikeSharp.API.Core;
+﻿using CounterStrikeSharp.API.Core;
 using FreakStrike2.Models;
-using Microsoft.Extensions.Logging;
 
-namespace FreakStrike2
+namespace FreakStrike2;
+public partial class FreakStrike2
 {
-    public partial class FreakStrike2
+    /// <summary>
+    /// 이벤트 등록
+    /// </summary>
+    private void GameEventInitialize()
     {
-        private void GameEventInitialize()
-        {
-            RegisterEventHandler<EventRoundStart>(OnRoundStart);
-            RegisterEventHandler<EventRoundEnd>(OnRoundEnd);
-            
-            RegisterListener<Listeners.OnMapStart>(OnMapStart);
-            RegisterListener<Listeners.OnClientPutInServer>(OnClientPutInServer);
-            RegisterListener<Listeners.OnClientDisconnect>(OnClientDisconnect);
-            RegisterListener<Listeners.OnTick>(OnTick);
-        }
+        RegisterEventHandler<EventRoundStart>(OnRoundStart);
+        RegisterEventHandler<EventRoundEnd>(OnRoundEnd);
+        
+        RegisterListener<Listeners.OnMapStart>(OnMapStart);
+        RegisterListener<Listeners.OnClientPutInServer>(OnClientPutInServer);
+        RegisterListener<Listeners.OnClientDisconnect>(OnClientDisconnect);
+        RegisterListener<Listeners.OnTick>(OnTick);
+    }
 
-        private void OnMapStart(string mapName)
-        {
-            KillGameTimer();
-        }
+    /// <summary>
+    /// 이벤트 제거
+    /// </summary>
+    private void GameEventDeregister()
+    {
+        DeregisterEventHandler<EventRoundStart>(OnRoundStart);
+        DeregisterEventHandler<EventRoundEnd>(OnRoundEnd);
 
-        private void OnClientPutInServer(int client)
-        {
-            GameStartOnClientPutInServer();
-            DebugDisableOnClientPutInServer(client);
-            TeamChangeOnClientPutInServer(client);
-            // var player = Utilities.GetPlayerFromSlot(client);
-            // if (player is not null && player.IsValid && !player.IsBot && !player.IsHLTV)
-            // {
-            //     Logger.LogInformation($"[FreakStrike2] Player {player.PlayerName} ({player.AuthorizedSteamID?.SteamId64}) is put in server with {player.Slot}.");
-            //
-            // }
-        }
+        RemoveListener(OnMapStart);
+        RemoveListener(OnClientPutInServer);
+        RemoveListener(OnClientDisconnect);
+        RemoveListener(OnTick);
+    }
 
-        private void OnClientDisconnect(int client)
-        {
-            
-        }
+    /// <summary>
+    /// 맵 시작
+    /// </summary>
+    /// <param name="mapName">맵 이름</param>
+    private void OnMapStart(string mapName)
+    {
+        KillGameTimer();
+        CreatePlayerQueuepoint();
+    }
 
-        private void OnTick()
-        {
-            DebugPrintGameCondition();
-        }
+    /// <summary>
+    /// 클라이언트 서버 접속
+    /// </summary>
+    /// <param name="client">클라이언트</param>
+    private void OnClientPutInServer(int client)
+    {
+        GameStartOnClientPutInServer();
+        DebugDisableOnClientPutInServer(client);
+        TeamChangeOnClientPutInServer(client);
+        // var player = Utilities.GetPlayerFromSlot(client);
+        // if (player is not null && player.IsValid && !player.IsBot && !player.IsHLTV)
+        // {
+        //     Logger.LogInformation($"[FreakStrike2] Player {player.PlayerName} ({player.AuthorizedSteamID?.SteamId64}) is put in server with {player.Slot}.");
+        //
+        // }
+    }
 
-        private HookResult OnRoundStart(EventRoundStart @event, GameEventInfo eventInfo)
-        {
-            RemoveEntities();
-            CreateGameTimer();
-            
-            return HookResult.Continue;
-        }
+    /// <summary>
+    /// 클라이언트 서버 퇴장
+    /// </summary>
+    /// <param name="client">클라이언트</param>
+    private void OnClientDisconnect(int client)
+    {
+        ResetQueuepointOnClientDisconnect(client);
+    }
 
-        private HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo eventInfo)
+    /// <summary>
+    /// 틱
+    /// </summary>
+    private void OnTick()
+    {
+        DebugPrintGameCondition();
+    }
+
+    /// <summary>
+    /// 라운드 시작
+    /// </summary>
+    /// <param name="event">이벤트</param>
+    /// <param name="eventInfo">이벤트 정보</param>
+    /// <returns>이벤트 훅</returns>
+    private HookResult OnRoundStart(EventRoundStart @event, GameEventInfo eventInfo)
+    {
+        RemoveEntities();
+        CreateGameTimer();
+        
+        return HookResult.Continue;
+    }
+
+    /// <summary>
+    /// 라운드 종료 
+    /// </summary>
+    /// <param name="event">이벤트</param>
+    /// <param name="eventInfo">이벤트 정보</param>
+    /// <returns>이벤트 훅</returns>
+    private HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo eventInfo)
+    {
+        if (_gameStatus is GameStatus.PlayerWaiting)
         {
-            if (_gameStatus is GameStatus.PlayerWaiting)
-                return HookResult.Stop;
-            
-            _gameStatus = GameStatus.End;
-            
-            return HookResult.Continue;
+            return HookResult.Handled;
         }
+        
+        _gameStatus = GameStatus.End;
+        _queuepoint.CalculatePlayerQueuepoints(_halePlayers);
+        
+        return HookResult.Continue;
     }
 }
