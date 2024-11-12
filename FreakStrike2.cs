43e8512465361a1c590cab775428d45c4f662df6
qufnr <qufnr@kakao.com>
2024-11-13 01:42:20 +0900

    main : Load() 로직 변경

--- FreakStrike2.cs
+++ FreakStrike2.cs
@@ -6,25 +6,26 @@ public partial class FreakStrike2 : BasePlugin, IPluginConfig<GameConfiguration>
     public override string ModuleName => "FreakStrike2";
     public override string ModuleVersion => "1.0.0";
     public override string ModuleAuthor => "byeoruk";
-    public override string ModuleDescription => "FF2 in CS2.";
+    public override string ModuleDescription => "Freak Fortress 2 in Counter-Strike 2.";
 
     public override void Load(bool hotReload)
     {
         Console.WriteLine("[FreakStrike2] Freak-Strike 2 loaded!");
-
-        GameEventInitialize();
-        // ServerCommandInitialize();
-
+        
         if (hotReload)
         {
             IgnoreRoundWinConditions();
+            GameResetOnHotReload();
+            GameEventDeregister();
         }
+        
+        GameEventInitialize();
+        GetHaleJsonOnLoad(hotReload);
+        // ServerCommandInitialize();
     }
 
     public override void OnAllPluginsLoaded(bool hotReload)
     {
         ServerCommandInitialize();
     }
-    
-    
 }
