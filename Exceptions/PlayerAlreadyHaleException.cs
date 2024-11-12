ad577beac659b4d274da8e4657217e38a6e6a543
qufnr <qufnr@kakao.com>
2024-11-13 01:36:49 +0900

    exception : 사용자 정의 예외 추가

---
+++ Exceptions/PlayerAlreadyHaleException.cs
@@ -0,0 +1,9 @@
+﻿namespace FreakStrike2.Exceptions;
+
+public class PlayerAlreadyHaleException : Exception
+{
+    public PlayerAlreadyHaleException() : base("해당 플레이어는 이미 헤일입니다.")
+    {
+        
+    }
+}
---
+++ Exceptions/PlayerNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace FreakStrike2.Exceptions;
+
+public class PlayerNotFoundException : Exception
+{
+    public PlayerNotFoundException() : base("플레이어를 찾을 수 없습니다.")
+    {
+        
+    }
+}
