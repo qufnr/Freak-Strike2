49429565162febed5c707e910167fd5a3be8ed0f
qufnr <qufnr@kakao.com>
2024-11-18 11:47:40 +0900

    utils : 서버 유틸 추가

---
+++ Utils/ServerUtils.cs
@@ -0,0 +1,33 @@
+﻿using CounterStrikeSharp.API;
+
+namespace FreakStrike2.Utils;
+
+public class ServerUtils
+{
+    /// <summary>
+    /// 모두에게 PrintToCenter
+    /// </summary>
+    /// <param name="message">메시지</param>
+    public static void PrintToCenterAll(string message) =>
+        Utilities.GetPlayers().Where(player => player.IsValid && !player.IsBot)
+            .ToList()
+            .ForEach(player => player.PrintToCenter(message));
+
+    /// <summary>
+    /// 모두에게 PrintToCenterHtml
+    /// </summary>
+    /// <param name="message">메시지</param>
+    public static void PrintToCenterHtmlAll(string message) =>
+        Utilities.GetPlayers().Where(player => player.IsValid && !player.IsBot)
+            .ToList()
+            .ForEach(player => player.PrintToCenterHtml(message));
+    
+    /// <summary>
+    /// 모두에게 PrintToCenterAlert
+    /// </summary>
+    /// <param name="message">메시지</param>
+    public static void PrintToCenterAlertAll(string message) =>
+        Utilities.GetPlayers().Where(player => player.IsValid && !player.IsBot)
+            .ToList()
+            .ForEach(player => player.PrintToCenterAlert(message));
+}
