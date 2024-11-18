a4a7436a99570d391e92ec64910f946be8038d5e
qufnr <qufnr@kakao.com>
2024-11-18 11:47:54 +0900

    utils : 플레이어 유틸 추가

---
+++ Utils/PlayerUtils.cs
@@ -0,0 +1,12 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+
+namespace FreakStrike2.Utils;
+
+public class PlayerUtils
+{
+    public static List<CCSPlayerController> GetValidPawnAlivePlayers() => Utilities.GetPlayers().Where(p => p.IsValid && p.PawnIsAlive).ToList();
+
+    public static CCSPlayerController? GetRandomPlayer() =>
+        GetValidPawnAlivePlayers().Where(p => p.Slot == CommonUtils.GetRandomInt(0, Server.MaxPlayers)).FirstOrDefault();
+}
