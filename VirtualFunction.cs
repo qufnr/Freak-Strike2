edda1bdb580abb6ec66aa6d1609af9db6dc6ad37
qufnr <qufnr@kakao.com>
2024-11-20 16:50:01 +0900

    virtual function : CanAcquireFunc 훅

---
+++ VirtualFunction.cs
@@ -0,0 +1,30 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Memory;
+using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+
+namespace FreakStrike2;
+
+public partial class FreakStrike2
+{
+    private void HookVirtualFunctions()
+    {
+        VirtualFunctions.CCSPlayer_ItemServices_CanAcquireFunc.Hook(OnWeaponCanAcquire, HookMode.Pre);
+    }
+
+    private void UnhookVirtualFunctions()
+    {
+        VirtualFunctions.CCSPlayer_ItemServices_CanAcquireFunc.Unhook(OnWeaponCanAcquire, HookMode.Pre);
+    }
+
+    private HookResult OnWeaponCanAcquire(DynamicHook hook)
+    {
+        var player = hook.GetParam<CCSPlayer_ItemServices>(0).Pawn.Value.Controller.Value!.As<CCSPlayerController>();
+        if (!player.IsValid || !player.PawnIsAlive)
+            return HookResult.Continue;
+
+        if (BaseHalePlayers[player.Slot].IsHale())
+            return HookResult.Stop;
+        
+        return HookResult.Continue;
+    }
+}
