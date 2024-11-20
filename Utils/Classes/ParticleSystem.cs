05b949e7563194de490eb27656ebf1ae58ec3ed9
qufnr <qufnr@kakao.com>
2024-11-20 17:22:44 +0900

    particle system : 파티클 생성, 수정 관리하는 클래스 추가 (테스트 안해봄)

---
+++ Utils/Classes/ParticleSystem.cs
@@ -0,0 +1,53 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+using FreakStrike2.Utils.Exceptions;
+
+namespace FreakStrike2.Utils.Classes;
+
+public class ParticleSystem
+{
+    public CParticleSystem Particle { get; private set; }
+
+    public ParticleSystem(string effectName)
+    {
+        var particle = Utilities.CreateEntityByName<CParticleSystem>("info_particle_system");
+        if (particle == null)
+            throw new GameNotSupportedException();
+        
+        particle.EffectName = effectName;
+        Particle = particle;
+    }
+
+    public void SetParent(CBaseEntity entity)
+    {
+        Particle.Teleport(entity.AbsOrigin);
+        Particle.AcceptInput("FollowEntity", Particle, entity, "!activator");
+    }
+
+    public void SetParent(CCSPlayerController player)
+    {
+        var playerPawn = player.PlayerPawn.Value;
+        if (playerPawn is null)
+            throw new NullReferenceException("Member Name \"m_hPlayerPawn\" Value is null.");
+        
+        Particle.Teleport(playerPawn.AbsOrigin);
+        Particle.AcceptInput("FollowEntity", Particle, playerPawn, "!activator");
+    }
+
+    public void SetPoint(Vector end, Vector? start)
+    {
+        Particle.DataCP = 1;
+        Particle.DataCPValue.X = end.X;
+        Particle.DataCPValue.Y = end.Y;
+        Particle.DataCPValue.Z = end.Z;
+        Utilities.SetStateChanged(Particle, "CParticleSystem", "m_vecDataCPValue");
+        Particle.Teleport(start);
+        Server.RunOnTick(Server.TickCount + 1, () => Particle.AcceptInput("Start"));
+    }
+
+    public void Remove()
+    {
+        Particle.Remove();
+    }
+}
