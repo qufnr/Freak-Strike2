df94015e9a05d308117eeb2106439bcb8d78d3e9
qufnr <qufnr@kakao.com>
2024-11-22 18:03:00 +0900

    player : BaseGamePlayer.AddPlayerDamage() 메소드 제거

--- Classes/BaseGamePlayer.cs
+++ Classes/BaseGamePlayer.cs
@@ -7,6 +7,7 @@ namespace FreakStrike2.Classes;
 public class BaseGamePlayer
 {
     public int Damages { get; set; }            //  플레이어가 입힌 피해량
+    // public bool IsStunned { get; private set; } //  스턴 상태 여부
     public bool DebugMode { get; set; }         //  디버그 모드 활성화 여부
 
     public BaseGamePlayer()
@@ -23,22 +24,5 @@ public class BaseGamePlayer
     {
         DebugMode = !DebugMode;
         return DebugMode;
-    }
-    
-    /// <summary>
-    /// 플레이어의 입힌 피해량을 추가합니다. (OnPlayerHurt)
-    /// </summary>
-    /// <param name="victim">피해자</param>
-    /// <param name="attacker">가해자</param>
-    /// <param name="baseHalePlayers">플레이어 헤일 정보</param>
-    /// <param name="gameStatus">게임 상태</param>
-    /// <param name="damage">피해량</param>
-    public void AddPlayerDamage(CCSPlayerController? victim, CCSPlayerController? attacker, Dictionary<int, BaseHalePlayer> baseHalePlayers, GameStatus gameStatus, int damage)
-    {
-        if (victim is not null && attacker is not null &&
-            gameStatus is GameStatus.Start &&
-            baseHalePlayers[victim.Slot].IsHale &&
-            !baseHalePlayers[attacker.Slot].IsHale)
-            Damages += damage;
     }
 }
