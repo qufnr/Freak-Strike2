77326bac4c3d9a77295880405c23533b0d76d8f5
qufnr <qufnr@icloud.com>
2024-11-25 00:04:31 +0900

    utils : PlayerUtils 에 SetPlayerProgressBar 및 RemovePlayerPRogressBar 추가

--- Utils/PlayerUtils.cs
+++ Utils/PlayerUtils.cs
@@ -143,6 +143,11 @@ public class PlayerUtils
         Utilities.SetStateChanged(playerPawn, "CBaseEntity", "m_CBodyComponent");
     }
 
+    /// <summary>
+    /// 플레이어 움직임 유형을 설정합니다.
+    /// </summary>
+    /// <param name="player">플레이어 Pawn 객체</param>
+    /// <param name="moveType">움직임 유형</param>
     public static void SetPlayerMoveType(CCSPlayerPawn player, MoveType_t moveType)
     {
         player.MoveType = moveType;
@@ -150,6 +155,31 @@ public class PlayerUtils
         Utilities.SetStateChanged(player, "CBaseEntity", "m_MoveType");
     }
 
+    /// <summary>
+    /// 플레이어에게 프로그래스바를 생성합니다. (대테러부대일 경우 해체 UI, 테러리스트일 경우 폭탄설치 UI로 표시됩니다.)
+    /// </summary>
+    /// <param name="player">플레이어 Pawn 객체</param>
+    /// <param name="duration">시간</param>
+    public static void SetPlayerProgressBar(CCSPlayerPawn player, int duration)
+    {
+        player.ProgressBarDuration = duration;
+        player.ProgressBarStartTime = Server.CurrentTime;
+        Utilities.SetStateChanged(player, "CCSPlayerPawnBase", "m_iProgressBarDuration");
+        Utilities.SetStateChanged(player, "CCSPlayerPawnBase", "m_flProgressBarStartTime");
+    }
+
+    /// <summary>
+    /// 플레이어의 프로그래바를 제거합니다.
+    /// </summary>
+    /// <param name="player">플레이어 Pawn 객체</param>
+    public static void RemovePlayerProgressBar(CCSPlayerPawn player)
+    {
+        player.ProgressBarDuration = 0;
+        player.ProgressBarStartTime = 0;
+        Utilities.SetStateChanged(player, "CCSPlayerPawnBase", "m_iProgressBarDuration");
+        Utilities.SetStateChanged(player, "CCSPlayerPawnBase", "m_flProgressBarStartTime");
+    }
+    
     /// <summary>
     /// #UserId 혹은 플레이어 이름으로 플레이어 객체를 찾습니다.
     /// </summary>
