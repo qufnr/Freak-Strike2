bfcd8462206ac0eed46b5210de81c369e21eedaa
qufnr <qufnr@kakao.com>
2024-11-27 18:02:00 +0900

    human class : SetClass() 메소드 추가, Reset() 로직 변경

--- Classes/BaseHumanPlayer.cs
+++ Classes/BaseHumanPlayer.cs
@@ -15,6 +15,17 @@ public class BaseHumanPlayer
     /// <param name="slot">플레이어 슬롯</param>
     public BaseHumanPlayer(int slot = -1) => Reset(slot);
 
+    public void SetClass(CCSPlayerController player, BaseHuman human)
+    {
+        MyClass = human;
+        
+        if (player.PawnIsAlive)
+            player.CommitSuicide(false, true);
+        
+        if (!player.IsBot)
+            Server.PrintToChatAll($"{FreakStrike2.MessagePrefix}{player.PlayerName} 이(가) " + (MyClass != null ? $"클래스를 {human.Name} (으)로 변경했습니다." : $"{human.Name} 클래스를 선택했습니다."));
+    }
+    
     /// <summary>
     /// 스폰 시 플레이어의 클래스로 설정합니다.
     /// </summary>
@@ -43,7 +54,7 @@ public class BaseHumanPlayer
         if (slot != -1)
         {
             var player = Utilities.GetPlayerFromSlot(slot);
-            if (player != null && player.IsValid)
+            if (player != null && player.IsValid && !player.IsBot)
                 player.ChangeTeam(CsTeam.Spectator);
         }
     }
