98933adcbb39bc6a19e4638b1f4e733b6b7d0df9
qufnr <qufnr@kakao.com>
2024-11-27 11:57:20 +0900

    entity : 구매 지역 제거

--- Entity.cs
+++ Entity.cs
@@ -7,14 +7,12 @@ namespace FreakStrike2
     {
         private static void RemoveEntities()
         {
-            var entityNames = new List<string>() { "func_bomb_target", "func_hostage_rescue", "hostage_entity", "c4" };
+            var entityNames = new List<string>() { "func_bomb_target", "func_hostage_rescue", "hostage_entity", "c4", "func_buyzone" };
             foreach (var entityName in entityNames)
             {
                 var entities = Utilities.FindAllEntitiesByDesignerName<CEntityInstance>(entityName);
                 foreach (var entity in entities)
-                {
                     entity.Remove();
-                }
             }
         }
     }
