b37cd730ff714689bdaa2e754bf480589c82dc03
qufnr <qufnr@kakao.com>
2024-11-28 10:43:55 +0900

    console command : sv_disable_teamselect_menu 추가

--- ServerConsoleCommand.cs
+++ ServerConsoleCommand.cs
@@ -1,28 +1,26 @@
 ﻿using CounterStrikeSharp.API;
 
-namespace FreakStrike2
+namespace FreakStrike2;
+public partial class FreakStrike2
 {
-    public partial class FreakStrike2
+    private void ServerCommandInitialize()
     {
-        private void ServerCommandInitialize()
-        {
-            Server.ExecuteCommand("mp_restartgame 1");
-            
-            Server.ExecuteCommand("mp_limitteams 0");
-            Server.ExecuteCommand("mp_autoteambalance 0");
-            
-            Server.ExecuteCommand("mp_humanteam t");
-            
-            //  Bot Commands
-            Server.ExecuteCommand("bot_quota 0");
-            Server.ExecuteCommand("bot_quota_mode fill");
-            Server.ExecuteCommand("bot_kick");
-            Server.ExecuteCommand("bot_join_team t");
-        }
+        Server.ExecuteCommand("mp_restartgame 1");
+        Server.ExecuteCommand("mp_limitteams 0");
+        Server.ExecuteCommand("mp_autoteambalance 0");
+        Server.ExecuteCommand("mp_humanteam t");
+        
+        Server.ExecuteCommand("sv_disable_teamselect_menu 0");
+        
+        //  Bot Commands
+        Server.ExecuteCommand("bot_quota 0");
+        Server.ExecuteCommand("bot_quota_mode fill");
+        Server.ExecuteCommand("bot_kick");
+        Server.ExecuteCommand("bot_join_team t");
+    }
 
-        private void IgnoreRoundWinConditions()
-        {
-            Server.ExecuteCommand("mp_ignore_round_win_conditions 0");
-        }
+    private void IgnoreRoundWinConditions()
+    {
+        Server.ExecuteCommand("mp_ignore_round_win_conditions 0");
     }
 }
