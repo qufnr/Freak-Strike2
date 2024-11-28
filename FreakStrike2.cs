eae7c9639e08739397cf9f3fc274c42edcdd1ca5
qufnr <qufnr@kakao.com>
2024-11-28 15:14:33 +0900

    main : instance 선언

--- FreakStrike2.cs
+++ FreakStrike2.cs
@@ -8,6 +8,8 @@ using Timer = CounterStrikeSharp.API.Modules.Timers.Timer;
 namespace FreakStrike2;
 public partial class FreakStrike2 : BasePlugin, IPluginConfig<GameConfig>
 {
+    internal static FreakStrike2 Instance { get; private set; } = new();
+    
     public override string ModuleName => "FreakStrike2";
     public override string ModuleVersion => "1.0.0";
     public override string ModuleAuthor => "byeoruk";
@@ -19,8 +21,8 @@ public partial class FreakStrike2 : BasePlugin, IPluginConfig<GameConfig>
 
     public static string MessagePrefix = "[FS2] ";
     public static string LogMessagePrefix = "[FreakStrike2] ";
-    
-    public required GameConfig Config { get; set; }              //  플러그인 콘피그
+
+    public GameConfig Config { get; set; } = new(); //  플러그인 콘피그
     
     public List<BaseHale> Hales = new();            //  서버에서 설정한 헤일 List 객체
     public List<BaseHuman> Humans = new();          //  서버에서 설정한 인간 List 객체
@@ -31,13 +33,15 @@ public partial class FreakStrike2 : BasePlugin, IPluginConfig<GameConfig>
     public Timer? InGameRoundTimer = null;              //  라운드 타이머
     public int FindInterval = 0;                        //  헤일을 찾는 시간
 
-    public required Dictionary<int, BaseGamePlayer> BaseGamePlayers;    //  서버 내 플레이어 정보
-    public required Dictionary<int, BaseHalePlayer> BaseHalePlayers;    //  서버 내 플레이어 헤일 정보
-    public required Dictionary<int, BaseHumanPlayer> BaseHumanPlayers;  //  서버 내 플레이어 인간 클래스 정보
-    public required Dictionary<int, BaseQueuePoint> PlayerQueuePoints;  //  서버 내 플레이어 큐포인트 정보
+    public Dictionary<int, BaseGamePlayer> BaseGamePlayers = new(64);    //  서버 내 플레이어 정보
+    public Dictionary<int, BaseHalePlayer> BaseHalePlayers = new(64);    //  서버 내 플레이어 헤일 정보
+    public Dictionary<int, BaseHumanPlayer> BaseHumanPlayers = new(64);  //  서버 내 플레이어 인간 클래스 정보
+    public Dictionary<int, BaseQueuePoint> PlayerQueuePoints = new(64);  //  서버 내 플레이어 큐포인트 정보
     
     public override void Load(bool hotReload)
     {
+        Instance = this;
+        
         Console.WriteLine($"[FreakStrike2] Freak-Strike 2 ({ModuleVersion}) loaded!");
 
         if (hotReload)
