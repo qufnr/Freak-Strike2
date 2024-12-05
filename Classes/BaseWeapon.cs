fb96896f05259fe55a58c8a6f080689d6f788b0f
qufnr <qufnr@kakao.com>
2024-12-05 17:49:27 +0900

    weapon : 무기 구성 기본값 바인딩

--- Classes/BaseWeapon.cs
+++ Classes/BaseWeapon.cs
@@ -4,12 +4,12 @@ namespace FreakStrike2.Classes;
 
 public class BaseWeapon
 {
-    public int Clip { get; set; }
-    public int Ammo { get; set; }
-    public float KnockbackScale { get; set; }
-    public float KnockbackMaximumDistance { get; set; }
-    public float FireRate { get; set; }
-    public float Damage { get; set; }
+    public int Clip { get; set; } = 0;
+    public int Ammo { get; set; } = 0;
+    public float KnockbackScale { get; set; } = 1.0f;
+    public float KnockbackMaximumDistance { get; set; } = 800.0f;
+    public float FireRate { get; set; } = 1.0f;
+    public float Damage { get; set; } = 1.0f;
 
     public static Dictionary<string, BaseWeapon> GetWeaponsFromJson(string jsonString)
     {
